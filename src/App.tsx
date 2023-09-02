d78dda7ac9ee91c56bf6c5fa4293b17c088efe6d
Omkar0803 <omkarbdarde@gmail.com>
2023-09-02 18:03:33 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { Interface } from "readline";
+import InputField from "./components/InputField";
+import { Todo } from "./components/model";
+import Todolist from "./components/Todolist";
 
-function App() {
+// let name:string="Hello"
+// const age:number | string ="teenn"
+// const isNumber:boolean=true
+// const names:string[]=["abc","absn","12","safas","dsfd"]
+
+// type Person={
+// name:string;
+// age?:number;
+// }
+// const people:Person={
+//   name:"John"
+// }
+
+// let lotofPeople:Person[]=[people,people]
+
+// let printName:(name:string)=>void;
+
+// // *****************Type*************
+
+// type X ={
+//   a:string;
+//   b:number;
+// }
+// type Y = X & {
+//   c:string;
+//   d:number;
+// }
+
+// let y:Y={
+//   a:"abfg",
+//   b:23,
+//   c:"add",
+//   d:3
+
+// }
+
+// // *****************Interface*************
+// interface Man {
+//   name:string;
+//   age:number;
+
+// }
+
+// interface Guy extends Man{
+//   profession:string;
+// }
+
+const App: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleAdd = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (todo) {
+      setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+      setTodo("");
+    }
+  };
+  console.log(todos);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="heading">Taskifier</span>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
+      <Todolist todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/SingleTodo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Todo } from "./model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+
+type Props = {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+};
+
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id != id));
+  };
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+  return (
+    <form className="todos__single">
+      {todo.isDone ? (
+        <span className="todos__single--text">{todo.todo}</span>
+      ) : (
+        <s className="todos__single--text">{todo.todo}</s>
+      )}
+      <div>
+        <span className="icon">
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <AiFillDelete />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
